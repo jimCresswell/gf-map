b068a7d19cdc15bfb7b1e59b61611feedaa047cb
jimCresswell <jim.cresswell@gmail.com>
2014-12-23 10:56:02 +0000

    Get the user location with the geo location api and center the map on it.

--- js/main.js
+++ js/main.js
@@ -20,20 +20,40 @@ $(window).resize(setMapHeight);
 function go() {
     setMapHeight();
 
-    // COnfigure default markers.
+    // Configure default markers.
     L.Icon.Default.imagePath = 'node_modules/leaflet/dist/images'
 
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var geo_options = {
+      enableHighAccuracy: true
+    };
+    var wpid = navigator.geolocation.watchPosition(geo_success, geo_error, geo_options);
 
-    L.tileLayer('http://{s}.tiles.mapbox.com/v3/jimcresswell.kie2d4km/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18
-    }).addTo(map);
-
-    var marker = L.marker([51.5, -0.09]).addTo(map);
-    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
 }
 
 function setMapHeight(){
     $('#map').height($(document).height());
 };
+
+
+function geo_success(position) {
+  var geoLat = position.coords.latitude;
+  var geoLong = position.coords.longitude;
+
+  // 51.505, -0.09
+  var map = L.map('map').setView([geoLat, geoLong], 13);
+
+  L.tileLayer('http://{s}.tiles.mapbox.com/v3/jimcresswell.kie2d4km/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18
+  }).addTo(map);
+
+  var marker = L.marker([geoLat, geoLong]).addTo(map);
+  marker.bindPopup("You are here!").openPopup();
+}
+
+function geo_error() {
+  alert("Sorry, no position available.");
+}
+
+
+
